f0f4b2c7f1cdd1b282d19c67a24454d064ac41d9
redfire359 <redfire359@duck.com>
2024-12-12 00:43:48 -0500

    commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # LinuxHardeningScript
 bash script for out of the box ubuntu servers
+
+# WORK IN PROGRESS
+
+
+**This is designed for penetration testers setting up C2 server**
+
+This script does the following: 
+
+1. sets up auto updates
+2. installs and configures openvpn on port 1194
+3. hardens ssh and creates a new user with ssh key
+4. disables logging + shreds previous log files
+5. disables ping responses
+6. disables bash history files + shreds previous bash history files
---
+++ script.sh
@@ -0,0 +1,53 @@
+
+# Check if running as root 
+if [[ $UID  != 0 || $USER != "root" ]]; then
+    echo "Run it as root!"
+    exit
+fi 
+
+##### Set these values to whatever u want #####
+NEW_USER=user1 
+CLIENT_NAME=client1
+IP_SUBNET=10.10.10.0
+SUBNET_MASK=255.255.255.0
+########################################
+
+# Step 1 - update everything & enable auto updates.
+apt update -y 
+apt upgrade -y 
+
+if ! command unattended-upgrade 2>&1 >/dev/null 
+then 
+    echo "Installing unattended upgrade"
+    apt install unattended-upgrades -y 
+fi
+
+dpkg-reconfigure unattended-upgrades
+
+# Step 2 - download some additional binaries 
+packages=("wget" "openvpn" "easy-rsa" "fail2ban" "iptables" "sshd")
+for pkg in ${packages[@]}; do
+    
+    if ! command $pkg 2>&1 >/dev/null 
+    then 
+        apt install $pkg -y 
+    fi
+
+done
+
+# Setup openvpn 
+cd /usr/share/easy-rsa
+./easyrsa init-pki
+./easyrsa build-ca
+./easyrsa build-server-full
+./easyrsa build-client-full $CLIENT_NAME
+./easyrsa gen-dh
+
+openvpn --genkey tls-auth ta.key
+useradd openvpn 
+
+
+# Create openssh file
+
+# find /var/log/* -maxdepth 1 -type f 2>&/dev/null
+# iptables -I INPUT -p icmp --icmp-type echo-request -j DROP
